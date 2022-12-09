f16d9543c481884cda687923afb921fb462d8daa
Thomas <tailleurt@gmail.com>
2022-12-10 00:45:33 +0100

    Acceuil + profile et asso (vide)

--- app/src/main/java/fr/thomas/projet/Accueil.java
+++ app/src/main/java/fr/thomas/projet/Accueil.java
@@ -2,13 +2,49 @@ package fr.thomas.projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Accueil extends AppCompatActivity {
 
+    private View associations;
+    private View profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //----------- Creation de la page -----------
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.layout_accueil);
+        getSupportActionBar().hide();
+
+        //----------- Boutton rejoindre asso -----------
+
+        this.associations = findViewById(R.id.Associations);
+        associations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openActivity = new Intent(getApplicationContext(), Associations.class);
+                startActivity(openActivity);
+                finish();
+            }
+        });
+
+
+        //----------- Boutton profile -----------
+
+        this.profile = findViewById(R.id.Profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openActivity = new Intent(getApplicationContext(), Profile.class);
+                startActivity(openActivity);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/fr/thomas/projet/Associations.java
@@ -0,0 +1,18 @@
+package fr.thomas.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Associations extends AppCompatActivity {
+
+    private View associations;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_associations);
+    }
+}
