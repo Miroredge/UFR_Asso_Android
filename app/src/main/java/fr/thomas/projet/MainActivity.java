4050d0b8cfde631ab7a2b1f39c02f1ad12be5f83
Thomas <tailleurt@gmail.com>
2022-12-09 23:41:33 +0100

    Supression Login

--- app/src/main/java/fr/thomas/projet/MainActivity.java
+++ app/src/main/java/fr/thomas/projet/MainActivity.java
@@ -7,23 +7,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View inscription;
-    private View login;
+    private View retour;
+    private View accueil;
+    LinearLayout layout;
+    EditText email;
+    EditText password;
+
+    private String bddemail = "root";
+    private String bddmdp = "root";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //----------- Creation de la page -----------
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        setContentView(R.layout.layout_ouverture);
+        setContentView(R.layout.layout_login);
         getSupportActionBar().hide();
 
-        this.inscription = findViewById(R.id.inscription);
-        inscription.setOnClickListener(new View.OnClickListener() {
+
+
+        //----------- Création du text erreur -----------
+        this.layout = (TableLayout) findViewById(R.id.TableLayoutLogin);
+        TextView erreur = new TextView(this);
+        erreur.setText("Erreur: E-mail ou mot de pass incorect.");
+        erreur.setTextColor(getResources().getColor(R.color.bleuCustomFonce));
+        erreur.setTextSize(25);
+
+
+        //----------- Bouton S'inscrire -----------
+
+        this.retour = findViewById(R.id.RedirectionInscription);
+        retour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent openActivity = new Intent(getApplicationContext(), Inscription.class);
@@ -32,16 +54,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        this.login = findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+        //----------- Bouton connexion -----------
+
+        this.accueil = findViewById(R.id.valider);
+        accueil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent openActivity = new Intent(getApplicationContext(), Login.class);
-                startActivity(openActivity);
-                finish();
+                email = (EditText) findViewById(R.id.EmailLoginPageText);
+                password = (EditText) findViewById(R.id.PasswordLoginPageText);
+                if(email.getText().toString().equals("root") && password.getText().toString().equals("root")) {
+                    Intent openActivity = new Intent(getApplicationContext(), Accueil.class);
+                    startActivity(openActivity);
+                    finish();
+                }
+                else{
+                    layout.addView(erreur);
+                }
             }
         });
-
-
     }
 }
