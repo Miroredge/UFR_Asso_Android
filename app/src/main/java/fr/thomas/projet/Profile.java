6fbafa9cd999e3e0ab3e5b9a8f7bd17f485e2c16
MorganFreeman18 <102678753+MorganFreeman18@users.noreply.github.com>
2022-12-10 20:05:12 +0100

    lien discord profil

--- app/src/main/java/fr/thomas/projet/Profile.java
+++ app/src/main/java/fr/thomas/projet/Profile.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
 
     private View acceuil;
     private View associations;
     private View profile;
+    private TextView link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,5 +55,17 @@ public class Profile extends AppCompatActivity {
                 startActivity(openActivity);
             }
         });
+        
+        configuration_View();
+        set_Text();
+    }
+
+    private void set_Text() {
+        link.setText("https://discord.com/");
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void configuration_View() {
+         link = findViewById(R.id.link);
     }
 }
