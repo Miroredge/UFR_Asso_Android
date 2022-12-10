fea77fdc8d4c03812b243aed8967aff51da8f230
Thomas <tailleurt@gmail.com>
2022-12-10 01:36:44 +0100

    Register getTextInformations

--- app/src/main/java/fr/thomas/projet/Inscription.java
+++ app/src/main/java/fr/thomas/projet/Inscription.java
@@ -7,10 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class Inscription extends AppCompatActivity {
 
-    private View bienvenue;
+    private View login;
+    private View accueil;
+    LinearLayout layout;
+
+    EditText NomComplet;
+    EditText Email;
+    EditText NumTel;
+    EditText Pseudo;
+    EditText Mdp;
+    EditText MdpVerify;
+    CheckBox CheckBoxRegles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +36,47 @@ public class Inscription extends AppCompatActivity {
         setContentView(R.layout.layout_register);
         getSupportActionBar().hide();
 
-        this.bienvenue = findViewById(R.id.Retour);
-        bienvenue.setOnClickListener(new View.OnClickListener() {
+        //----------- Boutton connexion (Retour) -----------
+
+        this.login = findViewById(R.id.Retour);
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent openActivity = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(openActivity);
                 finish();
+            }
+        });
+
+        //----------- Création du text erreur -----------
+        this.layout = (LinearLayout) findViewById(R.id.LinearLayoutRegister);
+        TextView erreur = new TextView(this);
+        erreur.setText("Erreur: E-mail ou mot de pass incorect.");
+        erreur.setTextColor(getResources().getColor(R.color.bleuCustomFonce));
+        erreur.setTextSize(25);
+
+        //----------- Boutton s'inscrire -----------
+
+        this.accueil = findViewById(R.id.valider);
+        accueil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NomComplet = (EditText) findViewById(R.id.NomCompletRegisterPage);
+                Email = (EditText) findViewById(R.id.EmailRegisterPage);
+                NumTel = (EditText) findViewById(R.id.NumTelephoneRegisterPage);
+                Pseudo = (EditText) findViewById(R.id.PseudoRegisterPage);
+                Mdp = (EditText) findViewById(R.id.MotDePasseRegisterPage);
+                MdpVerify = (EditText) findViewById(R.id.VerificationMotDePasseRegisterPage);
+                CheckBoxRegles = (CheckBox) findViewById(R.id.CheckBoxRegles);
+                if(NomComplet.getText().toString().isEmpty() || Email.getText().toString().isEmpty() || NumTel.getText().toString().isEmpty()
+                || Pseudo.getText().toString().isEmpty() || Mdp.getText().toString().isEmpty() || MdpVerify.getText().toString().isEmpty() || !CheckBoxRegles.isChecked()){
+                    layout.addView(erreur);
+                }
+                else{
+                    Intent openActivity = new Intent(getApplicationContext(), Accueil.class);
+                    startActivity(openActivity);
+                    finish();
+                }
             }
         });
     }
