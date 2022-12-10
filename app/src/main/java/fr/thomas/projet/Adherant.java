c9aff23bc0a4b60b6af9432410b8dcfac7930317
MOttet18 <113054042+MOttet18@users.noreply.github.com>
2022-12-10 13:10:52 +0100

    activity_update

--- app/src/main/java/fr/thomas/projet/Adherant.java
+++ app/src/main/java/fr/thomas/projet/Adherant.java
@@ -3,12 +3,31 @@ package fr.thomas.projet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class Adherant extends AppCompatActivity {
+
+    GridView grid;
+
+    String[] tab = new String[40];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adherant);
+
+
+        for(int i=0;i<tab.length;i+=4){
+            tab[i]="Paul";
+            tab[i+1]="Janvier";
+            tab[i+2]="L2";
+            tab[i+3]="2022/11/18";
+        }
+
+        grid=(GridView)findViewById(R.id.grid);
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,tab);
+        grid.setAdapter(adapter);
+
     }
 }
