ad69c379f5698eb00790e1e624f8634a4f0d8c28
NikoGandon <nikola.gandon@gmail.com>
2022-12-11 21:56:57 +0100

    Nouveau lien BDD

--- app/src/main/java/fr/thomas/projet/Inscription.java
+++ app/src/main/java/fr/thomas/projet/Inscription.java
@@ -84,7 +84,7 @@ public class Inscription extends AppCompatActivity {
                 //MdpVerify.getText().toString();
                 //NumTel.getText().toString();
 
-                String url = "jdbc:mysql://astenor.freeboxos.fr:32800/ufr_asso";
+                String url = "jdbc:mysql://miroredge.freeboxos.fr:49999/ufr_asso";
 
 
                 try {
